ca4f9ca31c2fa8139f86f70b6c74c435d35a9c25
Kyle Taylor <kyletaylored@gmail.com>
2020-11-30 21:34:06 -0600

    Use nodesource for core Nodejs install

--- install.sh
+++ install.sh
@@ -72,40 +72,9 @@ cd piodome
 # Install dependencies
 pip install -r requirements.txt
 
-# Install Node based on Arm version.
-function install_node {
-	# Create local vars.
-	local dl_file="node.tar.xz"
-	local dl_dir="node_store"
-
-	# Download file to predermined file name.
-	wget -O $dl_file $1
-	# Create download directory and extract there.
-	mkdir $dl_dir
-	tar -xf -C $dl_dir $dl_file --strip 1
-	# Copy all files to /usr/local
-	sudo cp -r $dl_dir/* /usr/local/
-	# Cleanup
-	rm -rf $dl_dir
-	rm -f $dl_file
-}
-
-OS=`echo $(uname -m)`
-echo "${OS} detected..."
-case ${OS} in
-	armv6l)
-		install_node "https://nodejs.org/dist/v10.15.3/node-v10.15.3-linux-armv6l.tar.xz"
-	    ;;
-	armv7l)
-		install_node "https://nodejs.org/dist/v10.15.3/node-v10.15.3-linux-armv7l.tar.xz"
-	    ;;
-    arm64)
-		install_node "https://nodejs.org/dist/v10.15.3/node-v10.15.3-linux-arm64.tar.xz"
-	    ;;
-	*) echo "System not supported."
-	   exit 1
-	   ;;
-esac
+# Install Node
+curl -sL https://deb.nodesource.com/setup_14.x | sudo bash -
+sudo apt install nodejs
 
 if [[ $(type -t npm) != "" ]]; then
   echo "NPM exists. Continuing...";
